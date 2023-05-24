7942666fbe8a2a3dbd916731dc3b6a85ae88df33
Jesse Tuchsen <jesse@prlang.com>
2023-05-24 14:30:51 -0700

    Refactor to make the sequential queue of file transfers work

--- src/file_transfer.rs
+++ src/file_transfer.rs
@@ -20,69 +20,89 @@ use std::thread::{self,JoinHandle};
 use wry::application::event_loop::{EventLoopProxy};
 use fs_extra::{move_items_with_progress, copy_items_with_progress, dir::{CopyOptions,TransitProcessResult}};
 use crate::{FileTransferCmd,FileTransferCmdStart,FileTransferCmdResponse,FileTransferProgress,UserEvent};
+use std::collections::VecDeque;
 
 pub struct FileTransferService {
     proxy: EventLoopProxy<UserEvent>,
-    queue: Arc<Mutex<Vec<FileTransfer>>>
+    queue: Arc<Mutex<VecDeque<FileTransferCmdStart>>>,
+    running: Arc<Mutex<Option<Transfer>>>
 }
+
+struct Transfer(Sender<FileTransferCmdResponse>);
 
 impl FileTransferService {
     pub fn new(proxy: EventLoopProxy<UserEvent>) -> Self {
         Self {
             proxy,
-            queue: Arc::new(Mutex::new(vec![]))
+            queue: Arc::new(Mutex::new(VecDeque::new())),
+            running: Arc::new(Mutex::new(None))
         }
     }
 
     pub fn enqueue(&self, cmd: FileTransferCmdStart) {
-        let mut queue = self.queue.lock().unwrap();
-
-        if queue.len() == 0 {
-            let (snd, rec) = std::sync::mpsc::channel::<FileTransferCmdResponse>();
-            let proxy = self.proxy.clone();
-            let options = CopyOptions::default();
-            let handle = thread::spawn(move || match cmd {
-                FileTransferCmdStart::Copy { parent, names, to } => {
-                    let from: Vec<_> = names.into_iter().map(|name| parent.join(name)).collect();
-
-                    copy_items_with_progress(&from, &to, &options, move |progress| {
-                        let event =
-                            UserEvent::FileTransferProgress(FileTransferProgress::from(progress));
-
-                        proxy.send_event(event).unwrap();
-                        rec.recv().unwrap().into()
-                    }).unwrap();
-                },
-                FileTransferCmdStart::Cut { parent, names, to } => {
-                    let from: Vec<_> = names.into_iter().map(|name| parent.join(name)).collect();
-
-                    move_items_with_progress(&from, &to, &options, move |progress| {
-                        let event =
-                            UserEvent::FileTransferProgress(FileTransferProgress::from(progress));
-
-                        proxy.send_event(event).unwrap();
-                        rec.recv().unwrap().into()
-                    }).unwrap();
-                }
-            });
-
-            queue.push(FileTransfer::Running((snd, handle)));
-        } else {
-            queue.push(FileTransfer::Queued(cmd));
-        }
+        self.queue.lock().unwrap().push_back(cmd);
+        self.drain();
     }
 
     pub fn update(&self, response: FileTransferCmdResponse) {
-        let queue = self.queue.lock().unwrap();
-
-        match queue.first() {
-            Some(FileTransfer::Running((snd, _))) => snd.send(response).unwrap(),
-            _ => unreachable!()
+        match self.running.lock().unwrap().as_mut() {
+            Some(Transfer(sender)) => sender.send(response).unwrap(),
+            _ => {}
         }
+    }
+
+    fn drain(&self) {
+        if self.running.lock().unwrap().is_some() {
+            return;
+        }
+
+        let proxy = self.proxy.clone();
+        let queue = self.queue.clone();
+        let running = self.running.clone();
+
+        thread::spawn(move || {
+            while let Some(cmd) = queue.lock().unwrap().pop_front() {
+                let proxy = proxy.clone();
+                let (sender, receiver) = mpsc::channel::<FileTransferCmdResponse>();
+                let handle = spawn_file_transfer(cmd, receiver, proxy);
+
+                running.lock().unwrap().replace(Transfer(sender));
+                handle.join().unwrap();
+            }
+            *running.lock().unwrap() = None;
+        });
     }
 }
 
-enum FileTransfer {
-    Queued(FileTransferCmdStart),
-    Running((Sender<FileTransferCmdResponse>, JoinHandle<()>))
+fn spawn_file_transfer(
+    cmd: FileTransferCmdStart,
+    rec: Receiver<FileTransferCmdResponse>,
+    proxy: EventLoopProxy<UserEvent>) -> JoinHandle<()>
+{
+    let options = CopyOptions::default();
+
+    thread::spawn(move || match cmd {
+        FileTransferCmdStart::Copy { parent, names, to } => {
+            let from: Vec<_> = names.into_iter().map(|name| parent.join(name)).collect();
+
+            copy_items_with_progress(&from, &to, &options, move |progress| {
+                let event =
+                    UserEvent::FileTransferProgress(FileTransferProgress::from(progress));
+
+                proxy.send_event(event).unwrap();
+                rec.recv().unwrap().into()
+            }).unwrap();
+        },
+        FileTransferCmdStart::Cut { parent, names, to } => {
+            let from: Vec<_> = names.into_iter().map(|name| parent.join(name)).collect();
+
+            move_items_with_progress(&from, &to, &options, move |progress| {
+                let event =
+                    UserEvent::FileTransferProgress(FileTransferProgress::from(progress));
+
+                proxy.send_event(event).unwrap();
+                rec.recv().unwrap().into()
+            }).unwrap();
+        }
+    })
 }
