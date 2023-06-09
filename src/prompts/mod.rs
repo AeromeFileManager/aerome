fa7a495b4336dc31b73926e9c83c4e472a259249
Jesse Tuchsen <jesse@prlang.com>
2023-06-09 12:21:18 -0700

    Add license notifications to prompt files

--- src/prompts/mod.rs
+++ src/prompts/mod.rs
@@ -1,2 +1,18 @@
+/*
+ * Copyright (c) 2023 Jesse Tuchsen
+ *
+ * This file is part of Aerome.
+ *
+ * Aerome is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, version 3 of the License.
+ *
+ * Aerome is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Aerome. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 pub const PROMPT_COMMUNICATE: &'static [u8] = include_bytes!("./communicate.pr");
 pub const PROMPT_SUMMARY: &'static [u8] = include_bytes!("./summary.pr");
